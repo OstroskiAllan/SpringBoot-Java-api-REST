50bfd9a9452d9facfd33137c78ca9ebe400c57da
Ostroski <allan_s11@hotmail.com>
2024-01-04 12:07:33 -0300

    Att - sql banco

--- bd-sql/Sql-organizze.sql
+++ bd-sql/Sql-organizze.sql
@@ -1,6 +1,7 @@
 create database organizze;
 use organizze;
 
+
 -- Tabela Usuario
 CREATE TABLE Usuario (
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -9,48 +10,74 @@ CREATE TABLE Usuario (
     senha VARCHAR(100) NOT NULL
 );
 
--- Tabela Tarefa
-CREATE TABLE Tarefa (
+-- Tabela Projeto
+CREATE TABLE Projeto (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    observacaoTarefa VARCHAR(200),
-    data DATE,
-    dataEntrega DATE,
-    usuario_id INT,
-    tabela_id INT
+    nome VARCHAR(45) NOT NULL,
+    observacoes VARCHAR(200),
+	data DATE,
+	dataFinal DATE
 );
+
+ALTER TABLE projeto DROP COLUMN datafinal;
 
 -- Tabela Tabela
 CREATE TABLE Tabela (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(45) NOT NULL,
-    observacoes VARCHAR(200),
-    projeto_id INT
+    projeto_id INT,
+    FOREIGN KEY (projeto_id) REFERENCES Projeto(id)
 );
 
--- Tabela Projeto
-CREATE TABLE Projeto (
+-- Tabela Tarefa
+CREATE TABLE Tarefa (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(45) NOT NULL,
-    observacoes VARCHAR(200)
+    observacaoTarefa VARCHAR(200) NOT NULL,
+    data DATE,
+    dataEntrega DATE,
+    usuario_id INT,
+    tabela_id INT,
+    FOREIGN KEY (usuario_id) REFERENCES Usuario(id),
+    FOREIGN KEY (tabela_id) REFERENCES Tabela(id)
 );
+-- Procedimento para criar tabelas padrão quando um novo projeto é inserido
+DELIMITER //
 
--- Adição de chaves estrangeiras
-ALTER TABLE Tarefa
-    ADD CONSTRAINT fk_tarefa_usuario FOREIGN KEY (usuario_id) REFERENCES Usuario(id),
-    ADD CONSTRAINT fk_tarefa_tabela FOREIGN KEY (tabela_id) REFERENCES Tabela(id);
+CREATE PROCEDURE criar_tabelas_padrao(projeto_id_param INT)
+BEGIN
+    -- Inserir a tabela "A fazer"
+    INSERT INTO Tabela (nome, projeto_id) VALUES ('A fazer', projeto_id_param);
 
-ALTER TABLE Tabela
-    ADD CONSTRAINT fk_tabela_projeto FOREIGN KEY (projeto_id) REFERENCES Projeto(id);
+    -- Inserir a tabela "Em progresso"
+    INSERT INTO Tabela (nome, projeto_id) VALUES ('Em progresso', projeto_id_param);
 
+    -- Inserir a tabela "Pronto"
+    INSERT INTO Tabela (nome, projeto_id) VALUES ('Pronto', projeto_id_param);
+END //
+
+DELIMITER ;
+
+-- Trigger para chamar o procedimento quando um novo projeto é inserido
+DELIMITER //
+
+CREATE TRIGGER projeto_after_insert
+AFTER INSERT
+ON Projeto FOR EACH ROW
+BEGIN
+    CALL criar_tabelas_padrao(NEW.id);
+END //
+
+DELIMITER ;
 
 -- Inserção de dados fictícios na tabela Usuario
 INSERT INTO Usuario (nome, email, senha) VALUES
-    ('João', 'joao@email.com', '123'),
-    ('Maria', 'maria@email.com', '123'),
-    ('Carlos', 'carlos@email.com', '123'),
-    ('Ana', 'ana@email.com', '123'),
-    ('Paulo', 'paulo@email.com', '123');
+    ('João', 'joao@email.com', 'senha123'),
+    ('Maria', 'maria@email.com', 'senha456'),
+    ('Carlos', 'carlos@email.com', 'senha789'),
+    ('Ana', 'ana@email.com', 'senha101'),
+    ('Paulo', 'paulo@email.com', 'senha202');
 
+-- Inserção de dados fictícios na tabela Projeto
 INSERT INTO Projeto (nome, observacoes) VALUES
     ('Desenvolvimento de Aplicativo Móvel', 'Projeto para criar um aplicativo inovador para dispositivos móveis.'),
     ('Migração para a Nuvem', 'Projeto para migrar os sistemas existentes para uma infraestrutura em nuvem.'),
@@ -58,14 +85,10 @@ INSERT INTO Projeto (nome, observacoes) VALUES
     ('Atualização de Website', 'Projeto para redesenhar e atualizar o site corporativo.'),
     ('Integração de Sistemas', 'Projeto para integrar sistemas e melhorar a eficiência operacional.');
 
-
 -- Inserção de dados fictícios na tabela Tabela
-INSERT INTO Tabela (nome, observacoes, projeto_id) VALUES
-    ('Analise', 'Descrição da Tabela 1', 1),
-    ('A fazer', 'Descrição da Tabela 2', 2),
-    ('Fazendo', 'Descrição da Tabela 3', 3),
-    ('Feito', 'Descrição da Tabela 4', 4),
-    ('Revisao', 'Descrição da Tabela 5', 5);
+INSERT INTO Tabela (nome, projeto_id) VALUES
+    ('Tabela 1', 1),
+    ('Tabela 2', 1);
     
 -- Inserção de dados fictícios na tabela Tarefa
 INSERT INTO Tarefa (observacaoTarefa, data, dataEntrega, usuario_id, tabela_id) VALUES
@@ -79,3 +102,4 @@ INSERT INTO Tarefa (observacaoTarefa, data, dataEntrega, usuario_id, tabela_id) 
     ('Estratégia de Marketing', '2023-08-01', '2023-08-20', 3, 3),
     ('Otimização de Desempenho', '2023-09-01', '2023-09-15', 4, 4),
     ('Documentação de Sistemas', '2023-09-20', '2023-10-10', 5, 5);
+
